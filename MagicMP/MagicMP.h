3b82d0cbc8837251390208ae866b5d1f625ce911
pepibumur <pepibumur@gmail.com>
2013-10-13 13:32:22 +0200

    Travis file created

--- MagicMP/MagicMP.h
+++ MagicMP/MagicMP.h
@@ -7,7 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
+//Typedefs
+typedef void(^errorBlock)(NSError *error);
+typedef BOOL(^invitationBlock)(MCPeerID *peerID,NSData *context);
 
-@interface MagicMP : NSObject
+
+@interface MagicMP : NSObject <MCNearbyServiceAdvertiserDelegate>
+
++(id)sharedMP;
+-(void)startAdvertisingWithPeer:(MCPeerID*)peer discoveryInfo:(NSDictionary*)discoveryInfo serviceType:(NSString*)servicetype withInvitationBlock:(invitationBlock)invitationBlock andError:(errorBlock)errorBlock;
+
+// Properties
+@property (nonatomic,strong) MCNearbyServiceAdvertiser *advertiserEntity;
+@property (nonatomic,strong) MCSession *advertiserSession;
+
+
 
 @end
--- MagicMP/MagicMP.m
+++ MagicMP/MagicMP.m
@@ -8,6 +8,79 @@
 
 #import "MagicMP.h"
 
+@interface MagicMP()
+@property (nonatomic, copy) errorBlock errorBlock;
+@property (nonatomic, copy) invitationBlock invitationBlock;
+
+@end
+
 @implementation MagicMP
 
+#pragma mark - Initializer
++(id)sharedMP{
+    static MagicMP *sharedMP = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedMP = [[self alloc] init];
+    });
+    return sharedMP;
+}
+
+#pragma mark - Advertiser
+-(void)startAdvertisingWithPeer:(MCPeerID*)peer discoveryInfo:(NSDictionary*)discoveryInfo serviceType:(NSString*)servicetype withInvitationBlock:(invitationBlock)invitationBlock andError:(errorBlock)errorBlock{
+    //Stopping the current advertisingEntity
+    if(self.advertiserEntity){
+        [self.advertiserEntity stopAdvertisingPeer];
+    }
+    self.advertiserEntity=nil;
+    
+    //Detecting if there's no error block
+    if(!errorBlock){
+        NSLog(@"MagicMP: Error block required");
+        return;
+    }
+    _errorBlock=errorBlock;
+    
+    //Detecting if there's no invitation block
+    if(!invitationBlock){
+        NSLog(@"MagicMP: Invitation block required");
+        return;
+    }
+    _invitationBlock=invitationBlock;
+    
+    //Detecting if the serviceType length is less than 15 characters
+    if (servicetype.length>15){
+        errorBlock([NSError errorWithDomain:@"serviceType length should be 1-15 characters" code:1 userInfo:nil]);
+        return;
+    }
+
+    
+    //Detecting if there's peerID
+    if(!peer){
+        errorBlock([NSError errorWithDomain:@"PeerID cannot be nil" code:1 userInfo:nil]);
+        return;
+    }
+    
+    //Initializing
+    _advertiserEntity = [[MCNearbyServiceAdvertiser alloc] initWithPeer:peer discoveryInfo:discoveryInfo serviceType:servicetype];
+    
+    //Setting delegate
+    _advertiserEntity.delegate=self;
+
+}
+
+
+#pragma mark - MCNearbyServiceAdvertiserDelegate
+- (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didNotStartAdvertisingPeer:(NSError *)error{
+    //Checking if the MCNearbyServiceAdvertiser is the same
+    if(advertiser == self.advertiserEntity){
+        self.errorBlock(error);
+    }
+}
+- (void)advertiser:(MCNearbyServiceAdvertiser *)advertiser didReceiveInvitationFromPeer:(MCPeerID *)peerID withContext:(NSData *)context invitationHandler:(void (^)(BOOL accept, MCSession *session))invitationHandler{
+    
+    //Calling invitabion block
+    BOOL peerAccepted=self.invitationBlock(peerID,context);
+    
+}
 @end
